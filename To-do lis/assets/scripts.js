a4dec59ad4fb91b540c9fa480b949caf12d377c8
ALEXANDRE DA SILVA SOUZA <alexandresouza9@gmail.com>
2022-06-23 15:52:59 -0300

    update

--- To-do lis/assets/scripts.js
+++ To-do lis/assets/scripts.js
@@ -1,12 +1,13 @@
 const form = document.getElementById('task-form');
-const tasklist = document.getElementById('task');
+const taskList = document.getElementById('tasks');
 
-form.onsubmit = function (e){
-    e.preventDefault();
-    const inputField = document.getElementById('task-input');
-    addTask(inputField.value);
-    form.requestFullscreen();
+form.onsubmit = function (e) {
+	e.preventDefault();
+	const inputField = document.getElementById('task-input');
+	addTask(inputField.value);
+	form.reset();
 };
+
 function addTask(description) {
 	const taskContainer = document.createElement('div');
 	const newTask = document.createElement('input');
